9dc897801319256ba840faac392d8dfb8ce51b4d
acoustic121 <41907901+acoustic121@users.noreply.github.com>
2026-06-03 19:19:07 +0530

    Integration: Add Vercel Analytics tracking to pages/_app.js

--- desipdf/pages/_app.js
+++ desipdf/pages/_app.js
@@ -2,6 +2,7 @@ import '../styles/globals.css'
 import { AuthProvider } from '../utils/useAuth'
 import { I18nProvider } from '../utils/i18n'
 import Layout from '../components/Layout'
+import { Analytics } from '@vercel/analytics/react'
 
 export default function App({ Component, pageProps }) {
   return (
@@ -9,6 +10,7 @@ export default function App({ Component, pageProps }) {
       <I18nProvider>
         <Layout>
           <Component {...pageProps} />
+          <Analytics />
         </Layout>
       </I18nProvider>
     </AuthProvider>
